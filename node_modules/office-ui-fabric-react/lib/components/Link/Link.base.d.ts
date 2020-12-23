7327105180f75be1b0e74f206528572a6a3558cc
Oscar Francisco Castañeda Villagrán <racso@Oscars-MacBook-Pro-2.local>
2020-12-23 11:43:26 -0600

    initial commit

---
+++ node_modules/office-ui-fabric-react/lib/components/Link/Link.base.d.ts
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { ILink, ILinkProps } from './Link.types';
+export declare class LinkBase extends React.Component<ILinkProps, {}> implements ILink {
+    private _link;
+    constructor(props: ILinkProps);
+    render(): JSX.Element;
+    focus(): void;
+    private _onClick;
+    private _adjustPropsForRootType;
+    private _getRootType;
+}
